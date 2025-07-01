d8714d5d15995c159cfdeccd166fa6f3dad8d8ef
el_maximo <matteusmaximof@gmail.com>
2025-07-01 16:19:48 -0300

    Build 03

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751397426|166114789';
+const CACHE_VERSION = '1751397578|317530371';
 /** @type {string} */
 const CACHE_PREFIX = 'enfquizz-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
